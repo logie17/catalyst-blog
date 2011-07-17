e80194d633e4e78e3d313f0d50268b2a118aeacd
Logan Bell <loganbell@gmail.com>
2011-07-16 22:59:04 -0700

    Initial commit of HTML view

---
+++ lib/Blog/View/HTML.pm
@@ -0,0 +1,37 @@
+package Blog::View::HTML;
+
+use strict;
+use warnings;
+
+use base 'Catalyst::View::TT';
+
+__PACKAGE__->config(
+    TEMPLATE_EXTENSION => '.tt',
+    render_die => 1,
+    INCLUDE_PATH => [ Blog->path_to( 'root', 'src' ) ],
+);
+
+=head1 NAME
+
+Blog::View::HTML - TT View for Blog
+
+=head1 DESCRIPTION
+
+TT View for Blog.
+
+=head1 SEE ALSO
+
+L<Blog>
+
+=head1 AUTHOR
+
+Logan Bell
+
+=head1 LICENSE
+
+This library is free software. You can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
+
+1;
